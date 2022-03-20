18e3da1241b779632b7eda65758c7e81366b3388
efe.ozyer <e.ozyer@papara.com>
2022-03-20 10:00:48 +0300

    Project structure created

---
+++ src/ChannelEngine.Assessment.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChannelEngine.Assessment.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/ChannelEngine.Assessment.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ChannelEngine.Assessment.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
